897bab489993d2ed5fa11458b1873f3c37f1b386
William Laney <williamlaney@yahoo.com>
2016-04-19 17:12:02 -0400

    adjusted frequies cut off and filter order

--- accel_vs_time.m
+++ accel_vs_time.m
@@ -1,12 +1,12 @@
-[x,y,z,datetime,temp] = ...
+[x,y,z,date_time,temp] = ...
     import_animal_tag_data_v2('yxz_test.TXT',1, 206);
 
 %shift time date and temp to be in line with accel
-recoded_times=find(~isnat(datetime));
+recoded_times=find(~isnat(date_time));
 for n=2:length(recoded_times);
     %shift date time
-    datetime(recoded_times(n)-1)=datetime(recoded_times(n));
-    datetime(recoded_times(n))=[];
+    date_time(recoded_times(n)-1)=date_time(recoded_times(n));
+    date_time(recoded_times(n))=[];
     
     %shift temp
     temp(recoded_times(n)-1)=temp(recoded_times(n));
@@ -15,14 +15,14 @@ end
 
 %refind recorded times with new indecies
 %time_interp=datetime;
-time_ind=find(~isnat(datetime));
+time_ind=find(~isnat(date_time));
 for n=1:(length(time_ind)-1);
-    time_diff=datetime(time_ind(n+1))-datetime(time_ind(n)); %diff between recorded times
+    time_diff=date_time(time_ind(n+1))-date_time(time_ind(n)); %diff between recorded times
     num_of_point=time_ind(n+1)-time_ind(n); %number of points that need to be interp
     time_increase=time_diff/num_of_point; %step size of interp
     %fill in the blank times
     for k =time_ind(n)+1:time_ind(n+1)
-        datetime(k)=datetime(k-1)+time_increase;
+        date_time(k)=date_time(k-1)+time_increase;
     end
 end
 
@@ -33,7 +33,7 @@ y(not_accel,:)=[];
 z(not_accel,:)=[];
 
 %remove first time stamp so vectors are the same size
-datetime(1:1)=[];
+date_time(1:1)=[];
 
 % %plot of the x data incudling gravity
 % figure(1)
@@ -59,7 +59,7 @@ datetime(1:1)=[];
 %implemtent a high pass Butterworth filter to remove gravitional data and
 %isolate linear acceration
 
-fc=0.25; %frequency cut off
+fc=0.1; %frequency cut off
 fo=4; %filter order
 
 %applie butter filter and output transfer function coffiecents
@@ -71,9 +71,9 @@ z_bf=filter(b,a,z);
 %plot of the x data befor and after bf
 figure(4)
 subplot(3,1,1) %comment if you want this plot on its own
-plot(datetime,x,'-','MarkerSize',14)
+plot(date_time,x,'-','MarkerSize',14)
 hold on
-plot(datetime,x_bf,'-','MarkerSize',14)
+plot(date_time,x_bf,'-','MarkerSize',14)
 ylabel('x (g''s)')
 xlabel('time (hh:mm:ss)')
 title('x accel data before and after Butterworth filter')
@@ -82,9 +82,9 @@ legend('before filter','after filter')
 %plot of the y data befor and after bf
 %figure(5) %uncomment this and comment nl if you want this plot on its own
 subplot(3,1,2)
-plot(datetime,y,'-','MarkerSize',14)
+plot(date_time,y,'-','MarkerSize',14)
 hold on
-plot(datetime,y_bf,'-','MarkerSize',14)
+plot(date_time,y_bf,'-','MarkerSize',14)
 ylabel('y (g''s)')
 xlabel('time (hh:mm:ss)')
 title('y accel data before and after Butterworth filter')
@@ -93,9 +93,9 @@ legend('before filter','after filter')
 %plot of the z data befor and after bf
 %figure(6) %uncomment this and comment nl if you want this plot on its own
 subplot(3,1,3)
-plot(datetime,z,'-','MarkerSize',14)
+plot(date_time,z,'-','MarkerSize',14)
 hold on
-plot(datetime,z_bf,'-','MarkerSize',14)
+plot(date_time,z_bf,'-','MarkerSize',14)
 ylabel('z (g''s)')
 xlabel('time (hh:mm:ss)')
 title('z accel data before and after Butterworth filter')
