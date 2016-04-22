a063b6b09cb0e50c934b4713351c16e40c1946cb
William Laney <williamlaney@yahoo.com>
2016-04-22 14:28:27 -0400

    Added brickwall filter and plot results

--- accel_vs_time.m
+++ accel_vs_time.m
@@ -1,6 +1,7 @@
 [x,y,z,date_time,temp] = ...
     import_animal_tag_data_v2('yxz_test.TXT',1, 206);
 
+%% Interpolatoin and data arangment
 %shift time date and temp to be in line with accel
 recoded_times=find(~isnat(date_time));
 for n=2:length(recoded_times);
@@ -35,6 +36,8 @@ z(not_accel,:)=[];
 %remove first time stamp so vectors are the same size
 date_time(1:1)=[];
 
+%% Inital Plots
+
 % %plot of the x data incudling gravity
 % figure(1)
 % plot(time_interp,x,'.','MarkerSize',14)
@@ -56,6 +59,7 @@ date_time(1:1)=[];
 % xlabel('time (hh:mm:ss)')
 % title('z accel data including gravity')
 
+%% Buuterwoth Filter
 %implemtent a high pass Butterworth filter to remove gravitional data and
 %isolate linear acceration
 
@@ -68,6 +72,7 @@ x_bf=filter(b,a,x);
 y_bf=filter(b,a,y);
 z_bf=filter(b,a,z);
 
+%% ButterWorth Plots
 %plot of the x data befor and after bf
 figure(4)
 subplot(3,1,1) %comment if you want this plot on its own
@@ -100,3 +105,43 @@ ylabel('z (g''s)')
 xlabel('time (hh:mm:ss)')
 title('z accel data before and after Butterworth filter')
 legend('before filter','after filter')
+
+%% Brick Wall filter
+sampling_rate=8;
+x_brick=brick_wall(x, date_time,sampling_rate);
+y_brick=brick_wall(y, date_time,sampling_rate);
+z_brick=brick_wall(z, date_time,sampling_rate);
+
+%% Brick Wall plots
+figure(7)
+subplot(3,1,1) 
+plot(date_time,x,'-','MarkerSize',14)
+hold on
+plot(date_time,x_brick,'-','MarkerSize',14)
+ylabel('x (g''s)')
+xlabel('time (hh:mm:ss)')
+title('x accel data before and after brick wall filter')
+legend('before filter','after filter')
+
+%plot of the y data befor and after brick wall
+%figure(5) %uncomment this and comment nl if you want this plot on its own
+subplot(3,1,2)
+plot(date_time,y,'-','MarkerSize',14)
+hold on
+plot(date_time,y_brick,'-','MarkerSize',14)
+ylabel('y (g''s)')
+xlabel('time (hh:mm:ss)')
+title('y accel data before and after brick wall filter')
+legend('before filter','after filter')
+
+%plot of the z data befor and after brick wall
+subplot(3,1,3)
+plot(date_time,z,'-','MarkerSize',14)
+hold on
+plot(date_time,z_brick,'-','MarkerSize',14)
+ylabel('z (g''s)')
+xlabel('time (hh:mm:ss)')
+title('z accel data before and after brick wall filter')
+legend('before filter','after filter')
+
+%%
