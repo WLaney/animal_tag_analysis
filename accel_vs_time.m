c26fe9ebd0edc67c6146e546123f0bcb83ed53ea
William Laney <williamlaney@yahoo.com>
2016-06-08 10:11:55 -0400

    Fixed bug where the times were not shifting into alingmnet propely time postion indices were chaning in loop so the postions of the time found before the loop was not longer vaild, but was still being used

--- accel_vs_time.m
+++ accel_vs_time.m
@@ -3,21 +3,27 @@
 
 %% Interpolatoin and data arangment
 %shift time date and temp to be in line with accel
-recoded_times=find(~isnat(date_time));
-for n=2:length(recoded_times);
+recorded_times=find(~isnat(date_time));
+for n=2:length(recorded_times);
     %shift date time
-    date_time(recoded_times(n)-1)=date_time(recoded_times(n));
-    date_time(recoded_times(n))=[];
+    date_time(recorded_times(n)-1)=date_time(recorded_times(n));
+    date_time(recorded_times(n))=NaT;
     
     %shift temp
-    temp(recoded_times(n)-1)=temp(recoded_times(n));
-    temp(recoded_times(n))=[];
+    temp(recorded_times(n)-1)=temp(recorded_times(n));
+    temp(recorded_times(n))=NaN;
     
     %shift pressure
-    pressure(recoded_times(n)-1)=temp(recoded_times(n));
-    pressure(recoded_times(n))=[];
+    pressure(recorded_times(n)-1)=temp(recorded_times(n));
+    pressure(recorded_times(n))=NaN;
     
 end
+
+%remove the postions data was shifted out of, leave the first one alone in
+%data_time bc it is needed for the interpolation
+date_time(recorded_times(2:end))=[];
+temp(recorded_times)=[];
+pressure(recorded_times)=[];
 
 %refind recorded times with new indecies
 time_ind=find(~isnat(date_time));
@@ -31,8 +37,11 @@ for n=1:(length(time_ind)-1);
     end
 end
 
+%remove first time stamp so vectors are the same size
+date_time(1:1)=[];
+
 %get rid off all bank rows
-not_accel=isnan(x(1:end));
+not_accel=isnan(x_accel(1:end));
 x_accel(not_accel,:)=[];
 y_accel(not_accel,:)=[];
 z_accel(not_accel,:)=[];
@@ -40,8 +49,6 @@ x_gyro(not_accel,:)=[];
 y_gyro(not_accel,:)=[];
 z_gyro(not_accel,:)=[];
 
-%remove first time stamp so vectors are the same size
-date_time(1:1)=[];
 
 % %% Inital Plots
 % 
