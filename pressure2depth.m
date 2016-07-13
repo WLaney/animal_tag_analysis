dcb711aa536344d4d4e1096018dd363b2fed567a
William Laney <williamlaney@yahoo.com>
2016-07-13 10:05:06 -0400

    handles matrix input

--- pressure2depth.m
+++ pressure2depth.m
@@ -1,9 +1,12 @@
 function [depth]=pressure2depth(pressure_mbar)
-%converts pressure in mbar to depth in m
-%in feture factor in temp for more accurate messurment
+%converts pressure in mbar to depth in m of brackish water
+
+%postive values are how deep underwater the sensor is, negative values
+%corrosponed to being out of the water
 
 %% Constants
-%atmospheric pressure at Wachapreague, VA today (Jul 12, 2016)
+%atmospheric pressure at Wachapreague, VA today (Jul 12, 2016), this is
+%slightly high. Expect daily variation of approx +/- 4in 
 p_atmosphere_in=30.1; %inHG
 
 %approx density of brachis water, note density changes with depth but we
@@ -18,11 +21,11 @@ mbar2pa=100; %1mbar=100pa;
 %% conversions
 %get everything into SI units
 p_atmosphere=p_atmosphere_in*in2pa;
-pressure=pressure_mbar*mbar2pa;
+pressure=pressure_mbar.*mbar2pa;
 
 %% calulate depth
-%P_fluid=rho*g*depth
 %P_total=P_fluid+P_atmosphere
-%therfore; depth=(P_total-P_atmosphere)/(rho*g)
+%P_fluid=rho*g*depth
+%depth=(P_total-P_atmosphere)/(rho*g)
 depth=(pressure-p_atmosphere)./(rho*g);
 end
