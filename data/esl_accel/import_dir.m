fc16730fa4917565d5f61001c826134e6cc33f9e
Ben <bdpowell@email.wm.edu>
2016-07-20 20:44:22 -0400

    CSV to .mat conversion happen at a far more reasonable pace (i.e. seconds instead of minutes).

--- data/esl_accel/import_dir.m
+++ data/esl_accel/import_dir.m
@@ -1,7 +1,8 @@
-function [ accel, times ] = import_dir( dirname, start_time )
+function [ cells ] = import_dir( dirname )
 % Import an entire directory into accelerometer and time data.
 % Time data is offset from start_time, and dirname is the name of
 % the directory to process.
+% TODO Describe cell structure
 listing = dir(dirname);
 listing = listing(3:end);
 
@@ -13,21 +14,5 @@ parfor i=1:length(listing)
     cells{i} = {accel, times};
     disp(strcat('Finished: ', listing(i).name));
 end
-
-disp('Saving intermediate data...');
-save(strcat(dirname, '-intermediate'), 'cells');
-
-%% Move data into one giant array
-accel = [];
-times = [];
-for i=1:length(cells)
-   accel = [accel ; cells{i}{1}];
-   times = [times ; cells{i}{2}];
-end
-
-times = start_time + seconds(times);
-
-disp('Saving monolithic data...');
-save(strcat(dirname, '-monolithic'), 'accel', 'times');
 
 end
--- data/esl_accel/import_file.m
+++ data/esl_accel/import_file.m
@@ -2,27 +2,17 @@ function [ accel, times ] = import_file( filename )
 % Import the data from an accelerometer CSV into a MATLAB matrix.
 % accel is 3 column vectors of [x,y,z] data.
 % times is a column vector of seconds since logging started.
-accel = [];
-times = [];
 fp = fopen(filename);
+
 if (fp == -1)
     error(strcat('Couldn''t open file ', filename))
 end
 
-while true
-   line = fgetl(fp);
-   if ~ischar(line) % eof
-       break
-   elseif line(1) == ';' % skip comments
-       continue
-   else % data
-       lcs = strsplit(line, ',');
-       times = [times ; str2double(lcs{1})];
-       accel = [accel ; [str2double(lcs{2}), str2double(lcs{3}), str2double(lcs{4})]];
-   end
-end
+cells = textscan(fp, '%f32%d16%d16%d16', 'Delimiter', ',', 'CommentStyle', ';');
 
-% why is there no str2int? Did I just not look?
-accel = int32(accel);
+accel = [cells{2}, cells{3}, cells{4}];
+times = cells{1};
+
 fclose(fp);
+
 end
