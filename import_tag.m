65249c846db32c4c561778ff0d560b639567e526
Ben <bdpowell@email.wm.edu>
2016-07-21 10:05:08 -0400

    Optimized import_tag to take seconds instead of minutes. Moved our giant Sharkduino data files into data/sharkduino and added it to gitignore.

--- import_tag.m
+++ import_tag.m
@@ -6,126 +6,28 @@ function [accel,gyro,date_time,temp,pressure,date_time_short] = import_tag(filen
 %   date_time called date_time_short without blank rows. The default value
 %   of short is false
 
-%% Initialize variables.
-tic
-delimiter = ',';
-startRow = 2;
-endRow = inf;
-if nargin < 2 %check if short info was provided, if it was not make it false
-    short=false;
+%% Import raw data
+fp = fopen(filename);
+if (fp == -1)
+   error(strcat('Could not open ', filename));
 end
 
-%% Read columns of data as strings:
-% For more information, see the TEXTSCAN documentation.
-formatSpec = '%s%s%s%s%s%s%s%s%s%[^\n\r]';
+cells = textscan(fp, '%f%f%f%f%f%f%{y-M-d H:m:s}D%f%f', 'Delimiter', ',', ...
+    'Headerlines', 1);
+accel = [cells{1}, cells{2}, cells{3}];
+gyro = [cells{4}, cells{5}, cells{6}];
+date_time = cells{7};
+temp = cells{8};
+pressure = cells{9};
 
-%% Open the text file.
-fileID = fopen(filename,'r');
+fclose(fp);
 
-%% Read columns of data according to format string.
-% This call is based on the structure of the file used to generate this
-% code. If an error occurs for a different file, try regenerating the code
-% from the Import Tool.
-dataArray = textscan(fileID, formatSpec, endRow(1)-startRow(1)+1, 'Delimiter', delimiter, 'HeaderLines', startRow(1)-1, 'ReturnOnError', false);
-for block=2:length(startRow)
-    frewind(fileID);
-    dataArrayBlock = textscan(fileID, formatSpec, endRow(block)-startRow(block)+1, 'Delimiter', delimiter, 'HeaderLines', startRow(block)-1, 'ReturnOnError', false);
-    for col=1:length(dataArray)
-        dataArray{col} = [dataArray{col};dataArrayBlock{col}];
-    end
-end
-
-%% Close the text file.
-fclose(fileID);
-
-%% Convert the contents of columns containing numeric strings to numbers.
-% Replace non-numeric strings with NaN.
-raw = repmat({''},length(dataArray{1}),length(dataArray)-1);
-for col=1:length(dataArray)-1
-    raw(1:length(dataArray{col}),col) = dataArray{col};
-end
-numericData = NaN(size(dataArray{1},1),size(dataArray,2));
-
-for col=[1,2,3,4,5,6,8,9]
-    % Converts strings in the input cell array to numbers. Replaced non-numeric
-    % strings with NaN.
-    rawData = dataArray{col};
-    for row=1:size(rawData, 1);
-        % Create a regular expression to detect and remove non-numeric prefixes and
-        % suffixes.
-        regexstr = '(?<prefix>.*?)(?<numbers>([-]*(\d+[\,]*)+[\.]{0,1}\d*[eEdD]{0,1}[-+]*\d*[i]{0,1})|([-]*(\d+[\,]*)*[\.]{1,1}\d+[eEdD]{0,1}[-+]*\d*[i]{0,1}))(?<suffix>.*)';
-        try
-            result = regexp(rawData{row}, regexstr, 'names');
-            numbers = result.numbers;
-            
-            % Detected commas in non-thousand locations.
-            invalidThousandsSeparator = false;
-            if any(numbers==',');
-                thousandsRegExp = '^\d+?(\,\d{3})*\.{0,1}\d*$';
-                if isempty(regexp(thousandsRegExp, ',', 'once'));
-                    numbers = NaN;
-                    invalidThousandsSeparator = true;
-                end
-            end
-            % Convert numeric strings to numbers.
-            if ~invalidThousandsSeparator;
-                numbers = textscan(strrep(numbers, ',', ''), '%f');
-                numericData(row, col) = numbers{1};
-                raw{row, col} = numbers{1};
-            end
-        catch me
-        end
-    end
-end
-
-% Convert the contents of columns with dates to MATLAB datetimes using date
-% format string.
-try
-    dates{7} = datetime(dataArray{7}, 'Format', 'y-M-d H:m:s', 'InputFormat', 'y-M-d H:m:s');
-catch
-    try
-        % Handle dates surrounded by quotes
-        dataArray{7} = cellfun(@(x) x(2:end-1), dataArray{7}, 'UniformOutput', false);
-        dates{7} = datetime(dataArray{7}, 'Format', 'y-M-d H:m:s', 'InputFormat', 'y-M-d H:m:s');
-    catch
-        dates{7} = repmat(datetime([NaN NaN NaN]), size(dataArray{7}));
-    end
-end
-
-anyBlankDates = cellfun(@isempty, dataArray{7});
-anyInvalidDates = isnan(dates{7}.Hour) - anyBlankDates;
-dates = dates(:,7);
-
-%% Split data into numeric and cell columns.
-rawNumericColumns = raw(:, [1,2,3,4,5,6,8,9]);
-
-%% Replace non-numeric cells with NaN
-R = cellfun(@(x) ~isnumeric(x) && ~islogical(x),rawNumericColumns); % Find non-numeric cells
-rawNumericColumns(R) = {NaN}; % Replace non-numeric cells
-
-%% Allocate imported array to column variable names
-ax = cell2mat(rawNumericColumns(:, 1));
-ay = cell2mat(rawNumericColumns(:, 2));
-az = cell2mat(rawNumericColumns(:, 3));
-gx = cell2mat(rawNumericColumns(:, 4));
-gy = cell2mat(rawNumericColumns(:, 5));
-gz = cell2mat(rawNumericColumns(:, 6));
-date_time = dates{:, 1};
-temp = cell2mat(rawNumericColumns(:, 7));
-pressure = cell2mat(rawNumericColumns(:, 8));
-
-%consolidate accel and gyro in to 2 matricies
-accel=[ax, ay, az];
-gyro=[gx, gy, gz];
-clear ax ay az gx gy gz %clear repative variables to free up space
-
-%% creat short versions, if needed
-%if short is requested creat the short time, pressure, and temp
+%% Create short versions if needed
+% Short date-time, temp, pressure (no NaTs)
 if short==true
-    %creat date_time file of only datetimes written by RTC with no blanks
-    %for the pressure/temp data
+    %Remove NaTs from date-time
     date_time_short=date_time(~isnat(date_time));
-    %the first row does not have a coroponding temp/pressure
+    %First row should be removed (no temp/pressure)
     date_time_short(1)=[];
     
     %get rid of all blank rows in pressure and temp
@@ -180,5 +82,5 @@ gyro(not_data,:)=[];
 %the SD, this does not occure simotanusly with any accel/gyro read
 date_time(not_data)=[];
 warning('Save workspace so you do not need to import the data again')
-toc
+
 end
